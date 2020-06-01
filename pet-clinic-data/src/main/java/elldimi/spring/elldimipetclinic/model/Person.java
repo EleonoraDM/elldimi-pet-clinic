b2b9d0952075bf75ca6aff665a065ba26419c607
EleonoraDM <elinor.dm@gmail.com>
2020-06-01 10:45:57 +0300

    add id property
    Closes #10

--- pet-clinic-data/src/main/java/elldimi/spring/elldimipetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/elldimi/spring/elldimipetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package elldimi.spring.elldimipetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/elldimi/spring/elldimipetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/elldimi/spring/elldimipetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package elldimi.spring.elldimipetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
