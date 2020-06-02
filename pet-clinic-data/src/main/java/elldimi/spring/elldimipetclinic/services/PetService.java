0314dcd3e5b8d72b95afe7fdc75449a68a1651ca
EleonoraDM <elinor.dm@gmail.com>
2020-06-02 15:24:21 +0300

    refactored service interfaces
    Closes #11

--- pet-clinic-data/src/main/java/elldimi/spring/elldimipetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/elldimi/spring/elldimipetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package elldimi.spring.elldimipetclinic.services;
 
 import elldimi.spring.elldimipetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
