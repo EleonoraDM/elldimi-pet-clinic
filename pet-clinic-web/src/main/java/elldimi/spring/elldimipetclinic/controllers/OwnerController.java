78ff3dc0e55952310090fc130702efb7842b35ec
EleonoraDM <elinor.dm@gmail.com>
2020-07-28 14:37:01 +0300

    Spring MVC - OwnerController and OwnersIndex page
    closes #16

--- pet-clinic-web/src/main/java/elldimi/spring/elldimipetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/elldimi/spring/elldimipetclinic/controllers/OwnerController.java
@@ -1,14 +1,26 @@
 package elldimi.spring.elldimipetclinic.controllers;
 
+import elldimi.spring.elldimipetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        //s: "owners" is the NAME of the attribute, which  will return list of owners!!!
+        //Thymeleaf will search for an attribute with exactly the same name,
+        // as it was written at the OwnersIndex page.
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
